0e1291831c3552991dd5e8eb5e2115f5ce8a65ee
Stephen Margheim <stephen.margheim@gmail.com>
2025-01-15 14:45:54 +0100

    Try to parse all SQL fixture files successfully

--- test/fixtures.test.rb
+++ test/fixtures.test.rb
@@ -2,22 +2,18 @@
 
 # Eventually, we should test a round-trip SQL -> AST -> SQL -> AST
 
-# require "timeout"
+require "timeout"
 
-# Dir.glob("./fixtures/**/*.sql") do |file_path|
-# 	test file_path do
-# 		sql = File.read(file_path)
+Dir.glob("./fixtures/**/*.sql") do |file_path|
+	test file_path do
+		sql = File.read(file_path)
 
-# 		begin
-# 			Timeout.timeout(1) do
-# 				Plume::Parser.new(sql).parse
-# 			end
-# 		rescue Timeout::Error
-# 			puts file_path
-# 		end
-# 	end
-# end
-
-test "it works" do
-	Plume::Parser.new("CREATE TABLE a1(message_id, name , UNIQUE(message_id, name) );").parse
+		begin
+			Timeout.timeout(1) do
+				Plume::Parser.new(sql).parse
+			end
+		rescue Timeout::Error
+			puts file_path
+		end
+	end
 end
